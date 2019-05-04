f744c388233a7ce00cba001810084d7f8751bb53
Sunil Kumar 19 <sunkumar19@publicisgroupe.net>
2019-05-04 16:01:40 +0530

    User Registration

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+connectDB();
+//Init middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('API running');
 });
+
+//Define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
